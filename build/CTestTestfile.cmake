04124e38c2a271cffe2ed530b4bbce1db3cefd05
Eli <eli.stuenkel@gmail.com>
2023-07-29 15:23:14 -0700

    ok i think we good now:

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eli/Documents/cProjects/SimpleNeuralNetwork
-# Build directory: /home/eli/Documents/cProjects/SimpleNeuralNetwork/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
